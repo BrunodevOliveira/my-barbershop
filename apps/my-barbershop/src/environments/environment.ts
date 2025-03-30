16bc479d7bad35473fe13a61b0fe0636e8380899
Bruno Oliveira <brunodevoliveira@gmail.com>
2025-03-30 14:31:40 -0300

    ✨ feat(environment): adicionar arquivos de ambiente e configurações para desenvolvimento

---
+++ apps/my-barbershop/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  SUPABASE_URL: 'https://zqvsvjvihqsmdahwucof.supabase.co',
+  SUPABASE_KEY:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpxdnN2anZpaHFzbWRhaHd1Y29mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI5MDU2NzUsImV4cCI6MjA1ODQ4MTY3NX0.drq42CM_NLzw8LeBxzesuYojluBg_zqaiBUYGj-xMuY',
+};
